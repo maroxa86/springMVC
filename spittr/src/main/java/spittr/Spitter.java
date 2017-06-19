fd98535bb3674d7a2ca9c82860c6d8ec3848a1ec
Xavier Roig Martín <maroxa86@gmail.com>
2017-06-19 23:56:41 +0200

    Validaciones de formulario

--- spittr/src/main/java/spittr/Spitter.java
+++ spittr/src/main/java/spittr/Spitter.java
@@ -1,15 +1,34 @@
 package spittr;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.validator.constraints.Email;
 
 public class Spitter {
 
   private Long id;
+  
+  @NotNull
+  @Size(min=5, max=16)
   private String username;
+  
+  @NotNull
+  @Size(min=5, max=25)
   private String password;
+  
+  @NotNull
+  @Size(min=2, max=30)
   private String firstName;
+  
+  @NotNull
+  @Size(min=2, max=30)
   private String lastName;
+  
+  @NotNull
+  @Email
   private String email;
 
   public Spitter() {}
--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -1,8 +1,11 @@
 package spittr.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +30,11 @@ public class SpitterController {
 	}
 	
 	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String processResgistration(Spitter spitter){
+	public String processResgistration(@Valid Spitter spitter, Errors errors){
+		
+		if(errors.hasErrors()){
+			return "registerForm";
+		}
 		spitterRepository.save(spitter);
 		return "redirect:/spitter/" + spitter.getUsername();
 	}
