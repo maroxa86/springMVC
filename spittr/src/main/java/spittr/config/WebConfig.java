d6b9f8fa9a07acd7fa8cb83c31260065e601c90b
Xavier Roig Martín <maroxa86@gmail.com>
2017-06-24 12:10:40 +0200

    Internacionalización

--- spittr/src/main/java/spittr/config/WebConfig.java
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -34,10 +36,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
-	      .addResourceHandler("/resources/**")
-	      .addResourceLocations("/resources/","/other-resources/")
-	      .setCachePeriod(5)
-	      .resourceChain(true)
-	      .addResolver(new PathResourceResolver());
+			.addResourceHandler("/resources/**")
+			.addResourceLocations("/resources/", "/other-resources/")
+			.setCachePeriod(5)
+			.resourceChain(true)
+			.addResolver(new PathResourceResolver());
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setCacheSeconds(10);
+		return messageSource;
 	}
 }
--- spittr/src/main/webapp/WEB-INF/views/home.jsp
+++ spittr/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 <%@ page session="false" %>
 <html>
   <head>
@@ -8,7 +9,7 @@
           href="<c:url value="/resources/style.css" />" >
   </head>
   <body>
-    <h1>Welcome to Spitter</h1>
+    <h1><s:message code="spittr.welcome"/></h1>
 
     <a href="<c:url value="/spittles" />">Spittles</a> | 
     <a href="<c:url value="/spitter/register" />">Register</a>
