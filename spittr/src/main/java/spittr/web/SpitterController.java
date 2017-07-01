8192a86a4bd0b157209ca5a77e0849801613a983
Xavier Roig Martín <maroxa86@gmail.com>
2017-07-01 11:39:09 +0200

    Utilización de atributos flash para enviar objetos entre solicitudes.

--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import spittr.Spitter;
 import spittr.data.SpitterRepository;
@@ -39,7 +40,7 @@ public class SpitterController {
   @RequestMapping(value="/register", method=POST)
   public String processRegistration(
 		 @Valid SpitterForm spitterForm, 
-		 Model model,
+		 RedirectAttributes model,
 		 Errors errors) throws IllegalStateException, IOException {
     if (errors.hasErrors()) {
       return "registerForm";
@@ -52,14 +53,17 @@ public class SpitterController {
     model.addAttribute("username", spitter.getUsername());
     //No forma parte de la URL de la redirección pero aparece en ella en forma de parametro en la petición GET
     model.addAttribute("spitterId", spitter.getId());
+    model.addFlashAttribute("spitter", spitter);
     return "redirect:/spitter/{username}";
   }
   
   @RequestMapping(value="/{username}", method=GET)
   public String showSpitterProfile(@PathVariable String username, Model model) {
-    Spitter spitter = spitterRepository.findByUsername(username);
-    model.addAttribute(spitter);
-    return "profile";
+	  if(!model.containsAttribute("spitter")){
+		  Spitter spitter = spitterRepository.findByUsername(username);
+		  model.addAttribute(spitter);
+	  }
+	  return "profile";
   }
   
 }
