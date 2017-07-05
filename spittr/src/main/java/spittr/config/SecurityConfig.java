404c9455c2f22afa16a96a952ce16c133e7efe04
Xavier Roig Martín <maroxa86@gmail.com>
2017-07-05 18:44:21 +0200

    Autentificación por bases de datos

--- spittr/src/main/java/spittr/config/SecurityConfig.java
+++ spittr/src/main/java/spittr/config/SecurityConfig.java
@@ -1,18 +1,35 @@
 package spittr.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user").password("password").roles("USER").and()
-			.withUser("admin").password("password").roles("USER", "ADMIN");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username, password, true"+
+								"from Spitter where username=?")
+			.authoritiesByUsernameQuery("select username, 'ROLE_USER' from Spitter where username=?")
+			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
 	}
+	
+	//Repositorio de usuarios en memoria
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//			.withUser("user").password("password").roles("USER").and()
+//			.withUser("admin").password("password").roles("USER", "ADMIN");
+//	}
 }
