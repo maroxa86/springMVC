af64ef7676e13d4edd6a67fa7298d1cc257addd5
Xavier Roig Martín <maroxa86@gmail.com>
2017-07-06 17:06:20 +0200

    Configuración de origen de datos agrupados, este ejemplo es con DBCP, tambien se puede hacer con C3P0 o BoneCP

--- spittr/src/main/java/spittr/config/DataConfig.java
+++ spittr/src/main/java/spittr/config/DataConfig.java
@@ -2,22 +2,24 @@ package spittr.config;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
 public class DataConfig {
 
 	@Bean
-	public JndiObjectFactoryBean dataSource(){
-		JndiObjectFactoryBean jndiObjectFB = new JndiObjectFactoryBean();
-		jndiObjectFB.setJndiName("jdbc/spittrDS");
-		jndiObjectFB.setResourceRef(true);
-		jndiObjectFB.setProxyInterface(javax.sql.DataSource.class);
-		return jndiObjectFB;
+	public BasicDataSource dataSource(){
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("org.h2.Driver");
+		ds.setUrl("jdbc:h2:tcp://localhost/~/spitter");
+		ds.setUsername("sa");
+		ds.setPassword(" ");
+		ds.setInitialSize(5);
+		return ds;
 	}
 	
 	@Bean
